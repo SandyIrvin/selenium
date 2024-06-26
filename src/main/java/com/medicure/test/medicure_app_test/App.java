ff6d916268d0f924a1927bafdbfa2a9d357386eb
Shivani <shivani.t@plus91labs.com>
2024-06-26 10:50:00 +0530

    Hello modifie

--- src/main/java/com/medicure/test/medicure_app_test/App.java
+++ src/main/java/com/medicure/test/medicure_app_test/App.java
@@ -2,12 +2,14 @@ package com.medicure.test.medicure_app_test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -21,17 +23,12 @@ public class App
 {
     public static void main( String[] args ) throws IOException
     {
-    	  //load driver
+    	      
+        
+        // Load driver
         WebDriverManager.chromedriver().setup();
-     	
-//          //setup configuration
-//        ChromeOptions chromeOptions = new ChromeOptions();
-//        WebDriver driver = new ChromeDriver(chromeOptions);
-//        System.out.println("Scripted Executing");
-//        driver.get("http://13.201.27.195:8082/contact.html");
-//        
-        
-        
+
+        // Setup configuration
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--headless=new"); // Use the new headless mode
         chromeOptions.addArguments("--disable-gpu"); // Disable GPU for headless mode
@@ -40,43 +37,46 @@ public class App
         chromeOptions.addArguments("--remote-debugging-port=9222"); // Add remote debugging port
         chromeOptions.addArguments("--disable-extensions"); // Disable extensions
         WebDriver driver = new ChromeDriver(chromeOptions);
-        System.out.println("Scripted Executing");
-      driver.get("http://13.201.27.195:8082/contact.html");
-      
-        
-        
-       
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-     	 //load application
-        driver.findElement(By.xpath("/html/body/section/div/div[2]/div[1]/div/div[1]/div[1]/div/input")).sendKeys("shivani"); 
-        driver.findElement(By.xpath("/html/body/section/div/div[2]/div[1]/div/div[1]/div[2]/div/input")).sendKeys("9876776659"); 
-        driver.findElement(By.xpath("/html/body/section/div/div[2]/div[1]/div/div[2]/input")).sendKeys("ss123@gamil.com");
-        driver.findElement(By.xpath("/html/body/section/div/div[2]/div[1]/div/div[3]/input")).sendKeys("Excellent");
-        
-      
-         //submit button to form
-        driver.findElement(By.xpath("/html/body/section/div/div[2]/div[1]/div/div[4]/button")).click();
-        
-        //response get
-        String response =driver.findElement(By.xpath("//*[@id=\"message\"]")).getText();
-        System.out.println(response);
-        
-        
-        //take screenshot
-        
-        TakesScreenshot scrShot = ((TakesScreenshot)driver);
-        
-        File scrFile =scrShot.getScreenshotAs(OutputType.FILE);
-        
-        File destFile = new File("test-report.jpg");
-        
-        FileUtils.copyFile(scrFile, destFile);
-        
-        
-    
-      
-        //validate response
-        driver.quit();
-     	
+        try {
+            System.out.println("Script Executing");
+            driver.get("http://13.201.27.195:8082/contact.html");
+
+            // Load application
+            WebElement nameField = driver.findElement(By.xpath("/html/body/section/div/div[2]/div[1]/div/div[1]/div[1]/div/input"));
+            nameField.sendKeys("shivani");
+
+            WebElement phoneField = driver.findElement(By.xpath("/html/body/section/div/div[2]/div[1]/div/div[1]/div[2]/div/input"));
+            phoneField.sendKeys("9876776659");
+
+            WebElement emailField = driver.findElement(By.xpath("/html/body/section/div/div[2]/div[1]/div/div[2]/input"));
+            emailField.sendKeys("ss123@gamil.com");
+
+            WebElement messageField = driver.findElement(By.xpath("/html/body/section/div/div[2]/div[1]/div/div[3]/input"));
+            messageField.sendKeys("Excellent");
+
+            // Submit button to form
+            WebElement submitButton = driver.findElement(By.xpath("/html/body/section/div/div[2]/div[1]/div/div[4]/button"));
+            submitButton.click();
+
+            // Get response
+            String response = driver.findElement(By.xpath("//*[@id='message']")).getText();
+            System.out.println(response);
+
+            // Take screenshot
+            TakesScreenshot scrShot = ((TakesScreenshot) driver);
+            File scrFile = scrShot.getScreenshotAs(OutputType.FILE);
+            File destFile = new File("target/surefire-reports/test-report.jpg");
+            FileUtils.copyFile(scrFile, destFile);
+
+            // Validate response
+            // Add your validation logic here, e.g., assert that response contains expected text
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
     }
 }
